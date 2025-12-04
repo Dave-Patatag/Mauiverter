9741305af5ea7a58e0b74313c5ca7ea2f0b536d1
ACT-STUDENT <ACT-STUDENT@LAB3-PC32>
2025-12-04 11:26:39 +0800

    Add project files.

---
+++ App.xaml.cs
@@ -0,0 +1,15 @@
+﻿namespace Maui_Converter
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+        }
+
+        protected override Window CreateWindow(IActivationState? activationState)
+        {
+            return new Window(new MainPage());
+        }
+    }
+}
